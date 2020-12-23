11b28bb9a2b56ca63e0443dde2e1fc82fb5d7d09
danielmedb <70508723+danielmedb@users.noreply.github.com>
2020-12-23 08:43:38 +0100

    readme updated

--- resources/views/posts/top.blade.php
+++ resources/views/posts/top.blade.php
@@ -20,17 +20,20 @@
             <button name="vote" value="down" class="btn-vote"><img src="{{ asset('images/arrow_up.png') }}" style="width: 10px;" /></button>
         </form>
         @endif
-
+        
     </div>
     
     <div class="title col-lg-10 pl-0"><a href="/post/{{$post->id}}">{{ $post->body }}</a>
         <span class="text-muted small">( {{ $post->source }})</span>
     </div>
-    <div class="under text-muted small pl-4"> 
-        {{ $post->votes->count() }} By {{ $post->user->name }} | {{ $post->created_at->diffForHumans() }} | <a href="/post/{{$post->id}}">{{ $post->comments->count() }} comments</a>
-    
-    </div>
-
+    <div class="under text-muted small pl-4"> {{ $post->votes->count() }} By {{ $post->user->name }} | {{ $post->created_at->diffForHumans() }} | <a href="/post/{{$post->id}}">{{ $post->comments->count() }} comments</a></div>
+    @can('delete', $post)
+    <form method="post" action="{{ route('posts.destroy', $post) }}">
+        @csrf
+        @method('DELETE')
+        <a type="submit" value="Delete">Delete</a>
+    </form>   
+    @endcan   
 </div>
 
 @endforeach
