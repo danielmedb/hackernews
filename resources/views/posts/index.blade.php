b689408b3f3e595ab99aea4143019eeb454480f4
danielmedb <70508723+danielmedb@users.noreply.github.com>
2021-01-01 17:39:22 +0100

    nav and paginate

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,42 +1,38 @@
 @extends('layouts.app')
-
 @section('content')
 
-
 @foreach($posts as $post)
+    <div class="row" style="background-color: #eaeaea;">
+        <div class="rank pr-2">{{ ($posts->currentPage()-1) * $posts->perPage() + $loop->index + 1 }}.</div>
+        <div class="vote pr-2">
+            @if(!$post->likedBy(auth()->user() ))
+            <form method="post" action="{{ route('posts.likes', $post) }}">
+                @csrf
+                <button class="btn-vote" name="vote" value="up"><img src="{{ asset('images/arrow_up.png') }}" style="width: 10px;" /></button>
+            </form>
+            @else
+            <form method="post" action="{{ route('posts.likes', $post) }}">
+                @csrf
+                @method('DELETE')
+                <button class="btn-vote" name="vote" value="down"><img src="{{ asset('images/arrow_up.png') }}" style="width: 10px;" /></button>
+                
+            </form>
+            @endif
+            @can('delete', $post)
+            <form method="post" action="{{ route('posts.destroy', $post) }}">
+                @csrf
+                @method('DELETE')
+                <button type="submit">Delete</button>
+            </form>   
+            @endcan   
+        </div>
+        
+        <div class="title col-10 pl-0"><a href="/post/{{$post->id}}">{{ $post->body }}</a>
+            <span class="text-muted small">( {{ $post->source }})</span>
+        </div>
+        <div class="under text-muted small pl-4"> {{ $post->votes->count()  }} {{ Str::plural('vote', $post->votes->count())  }} | By {{ $post->user->name }} {{ $post->created_at->diffForHumans() }} | <a href="/post/{{$post->id}}">{{ $post->comments->count() }} {{ Str::plural('comment', $post->comments->count()) }}</a></div>
 
-<div class="row" style="background-color: #eaeaea;">
-    <div class="rank pr-2">{{ $post->id }}.</div>
-    <div class="vote pr-2">
-        @if(!$post->likedBy(auth()->user() ))
-        <form method="post" action="{{ route('posts.likes', $post) }}">
-            @csrf
-            <button class="btn-vote" name="vote" value="up"><img src="{{ asset('images/arrow_up.png') }}" style="width: 10px;" /></button>
-        </form>
-        @else
-        <form method="post" action="{{ route('posts.likes', $post) }}">
-            @csrf
-            @method('DELETE')
-            <button class="btn-vote" name="vote" value="down"><img src="{{ asset('images/arrow_up.png') }}" style="width: 10px;" /></button>
-            
-        </form>
-        @endif
-        @can('delete', $post)
-        <form method="post" action="{{ route('posts.destroy', $post) }}">
-            @csrf
-            @method('DELETE')
-            <button type="submit">Delete</button>
-        </form>   
-        @endcan   
     </div>
-    
-    <div class="title col-lg-10 pl-0"><a href="/post/{{$post->id}}">{{ $post->body }}</a>
-        <span class="text-muted small">( {{ $post->source }})</span>
-    </div>
-    <div class="under text-muted small pl-4"> {{ $post->votes->count()  }} {{ Str::plural('vote', $post->votes->count())  }} | By {{ $post->user->name }} {{ $post->created_at->diffForHumans() }} | <a href="/post/{{$post->id}}">{{ $post->comments->count() }} {{ Str::plural('comment', $post->comments->count()) }}</a></div>
-
-</div>
-
 @endforeach
 
 {{ $posts->links() }}
