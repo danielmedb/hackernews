30105595f207584e9bef3acdf9cdd9ade66bc697
danielmedb <70508723+danielmedb@users.noreply.github.com>
2021-01-11 16:41:07 +0100

    update

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -17,5 +17,18 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_create_user_and_login()
+    {
+        // $user = User::factory()->create();
+        // $response = $this->actingAs($user)
+        //     ->get('/login');
+
+        $user = User::factory()->make();
+        $response = $this->get('/login');
+
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.login');
     }
 }
